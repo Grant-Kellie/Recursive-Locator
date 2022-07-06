3a447f82e68f31376aa298eba430bd1c92089e09
Cascade Media <50721672+Cascade-Media@users.noreply.github.com>
2022-07-06 20:17:50 +0100

    Amendment
    Returned incorrect file format

--- Examples/WordPress/block-patterns.php
+++ Examples/WordPress/block-patterns.php
@@ -1,7 +1,7 @@
 /**
  * @method CascadeIterator
  * Auto Searches for files and directories within a given directory
- * @return Json encoded
+ * @return Array
  * 
  * @author Grant Kellie <contact@cascade.media>
  * @copyright June 26 2022 Cascade Media
@@ -14,12 +14,9 @@ function photonyx_locate_block_patterns(){
 	];
 	$files = new Theme\Photonyx\CascadeRecursiveFileLocator();
 	$data = $files->compileFiles($paths, ['html', 'css', 'js', 'php', 'json']); // Locate all Files under $path
-	$decoded_data = $data;
-	
-	// json_decode($data);
 
 	$block_patterns = [];
-	foreach ($decoded_data as $data) {
+	foreach ($data as $data) {
 		array_push($block_patterns,$data['filename']);     
 	}
 
